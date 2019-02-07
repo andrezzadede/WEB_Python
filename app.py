717d4cadb267ecd88ebf910ce58995c969f7b47e
Andrezza <andrezza_la@outlook.com>
2019-02-06 23:17:02 -0200

    Alterações na porta

--- app.py
+++ app.py
@@ -89,7 +89,7 @@ def error404(error):
 
 if __name__ == '__main__':
     if os.environ.get('APP_LOCATION')=='heroku':
-        run(host='0.0.0.0', port=int(os.environ.get('PORT, 500')))
+        run(host='0.0.0.0', port=int(os.environ.get('PORT, 5000')))
     else:
         run(host='localhost', port=8080, debug=True, reloader=True)
 
